1b4702d933a91b2d81e3c486cb3133bc53dbdf2b
ChristinaRajakumari2005 <christinarajakumari0@gmail.com>
2026-07-16 11:53:26 +0530

    Added Update and Delete functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -28,7 +28,7 @@ def home():
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
 
-    cursor.execute("SELECT name, email, department FROM students")
+    cursor.execute("SELECT * FROM students")
     students = cursor.fetchall()
 
     conn.close()
@@ -47,14 +47,67 @@ def register():
     cursor = conn.cursor()
 
     cursor.execute(
-        "INSERT INTO students (name, email, department) VALUES (?, ?, ?)",
+        "INSERT INTO students(name,email,department) VALUES(?,?,?)",
         (name, email, department)
     )
 
     conn.commit()
     conn.close()
 
-    return home()
+    return redirect("/")
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM students WHERE id=?", (id,))
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/")
+
+
+@app.route("/edit/<int:id>")
+def edit(id):
+
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM students WHERE id=?", (id,))
+    student = cursor.fetchone()
+
+    conn.close()
+
+    return render_template("edit.html", student=student)
+
+
+@app.route("/update/<int:id>", methods=["POST"])
+def update(id):
+
+    name = request.form["name"]
+    email = request.form["email"]
+    department = request.form["department"]
+
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        UPDATE students
+        SET name=?, email=?, department=?
+        WHERE id=?
+        """,
+        (name, email, department, id)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
